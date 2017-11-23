288adf18c2680515b850cfec8880fb8b4bb80167
liumdlp <397922058@qq.com>
2017-11-23 10:31:47 +0800

    base controller  测试

--- application/app1/controller/base.php
+++ application/app1/controller/base.php
@@ -4,6 +4,31 @@ namespace frame3\app1;
  *
  */
 class base {
+	const BASE_ERR_CODE = 3330;
+	const ERR_MSG = [
+		self::BASE_ERR_CODE + 1 => '参数错误',
+	];
+
 	function __construct() {
+
+	}
+
+	public static function resp_suc(string $msg = '', $data = null) {
+		$resp = [
+			'code' => 0,
+			'msg' => ('' == $msg) ? 'Success' : $msg,
+			'data' => $data,
+		];
+		echo json_encode($resp);
+		return;
+	}
+
+	public function resp_err(int $errno = 0, string $msg = '') {
+
+
+	}
+
+	public function resp_raw($raw_data) {
+
 	}
 }
