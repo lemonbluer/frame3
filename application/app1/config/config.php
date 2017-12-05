88fa2385158c4236d3de6d656b24313a85626cbd
liumdlp <397922058@qq.com>
2017-12-05 11:37:42 +0800

    一主多从模式

--- application/app1/config/config.php
+++ application/app1/config/config.php
@@ -4,12 +4,28 @@ return [
 	[
 		'qipai_pingtai' =>
 		[
-			'type' => 'mysql',
-			'hostname' => '192.168.1.130',
-			'db_name' => 'app1',
-			'user' => 'root',
-			'password' => 'root',
+			'rw_seperate' => FALSE, // 读写分离模式（主从模式），只支持一主多从
 			'table_prefix' => 'f_',
-		],
+			'instances' => [
+				[
+					'is_master' => TRUE,
+					'type' => 'mysql',
+					'hostname' => '192.168.1.130',
+					'db_name' => 'app1',
+					'user' => 'root',
+					'password' => 'root',
+				],
+				[
+					'is_master' => FALSE,
+					'type' => 'mysql',
+					'hostname' => '192.168.1.130',
+					'db_name' => 'app1',
+					'user' => 'root',
+					'password' => 'root',
+				],
+			],
+		]
+		,
 	],
+	'appname' => 'xueji',
 ];
