06613c852ecf0681b97c603f099d6b617cbe5158
liumdlp <397922058@qq.com>
2017-12-21 18:06:41 +0800

    fix typo

--- core/default_config.php
+++ core/default_config.php
@@ -1,10 +1,10 @@
 <?php
 return [
-	'frame_name' => 'frame3',
-	'tunning_out_put_type' => 'HTML',
-	'default_app_name' => 'app1',
-	'debug_mode' => FLASE, // debug模式
-	'app_online' => TRUE,
-	'static_file_extension' => ['png', 'jpg', 'js', 'css', 'jpeg'],
-	'session_cookie_lifetime' => 259200,
+    'frame_name' => 'frame3',
+    'tunning_out_put_type' => 'HTML',
+    'default_app_name' => 'app1',
+    'debug_mode' => FALSE, // debug模式
+    'app_online' => TRUE,
+    'static_file_extension' => ['png', 'jpg', 'js', 'css', 'jpeg'],
+    'session_cookie_lifetime' => 259200,
 ];
